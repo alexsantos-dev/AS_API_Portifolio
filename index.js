c485840efc8e4d9337804f28d95bc8efc638b311
Alex Santos <alexsantos.web.developer@gmail.com>
2023-11-25 13:18:12 -0300

    ajustando o index - conexão com o banco bem sucedida!

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-import { Express } from "express";
+import express from "express";
+import conectDatabase from "./src/config/database.js";
+import dotenv from "dotenv"
+
+dotenv.config()
+
+const app = express()
+const porta = process.env.PORT || 3000
+
+conectDatabase()
+app.use(express.json())
+
+app.listen(porta, () => console.log(`Servidor rodando na porta: ${porta}`))
