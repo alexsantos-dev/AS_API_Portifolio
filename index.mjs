4e9af10e11c159613bc89e8d11a85c0bda25d1db
Alex Santos <alexsantos.web.developer@gmail.com>
2023-12-14 09:51:41 -0300

    teste de nova rota

--- index.mjs
+++ index.mjs
@@ -3,6 +3,7 @@ import conectDatabase from "./src/config/database.mjs";
 import dotenv from "dotenv"
 import ProjetoRouter from "./src/routes/projeto.routes.mjs";
 
+
 dotenv.config()
 
 const app = express()
@@ -11,5 +12,11 @@ const porta = process.env.PORT || 3000
 conectDatabase()
 app.use(express.json())
 app.use('/', ProjetoRouter);
+
+
+app.get('/', (req, res) => {
+    // Envia o script JavaScript para abrir uma nova aba e acessar o endereço especificado
+    res.send('<script>window.open("https://https://api-portifolio-ohio-br.onrender.com/recentes", "_blank");</script>');
+})
 
 app.listen(porta, () => console.log(`Servidor rodando na porta: ${porta}`))
