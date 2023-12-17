4f8483c433f7bfb32bf3e2278b3b504f5a820b8a
Δlex Santos <140655997+alexsantos-dev@users.noreply.github.com>
2023-12-17 20:32:12 -0300

    ajustando rotas

--- index.mjs
+++ index.mjs
@@ -11,12 +11,7 @@ const porta = process.env.PORT || 3000
 
 conectDatabase()
 app.use(express.json())
-app.use('/', ProjetoRouter);
-
-
-app.get('/', (req, res) => {
-    res.send('<script>window.open("https://api-portifolio-ohio-br.onrender.com/recentes", "_self");</script>');
-})
+app.use('/projetos', ProjetoRouter);
 
 async function api() {
     const accessUrl = async () => {
